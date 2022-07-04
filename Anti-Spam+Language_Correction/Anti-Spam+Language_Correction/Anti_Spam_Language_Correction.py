b2767e076d4151a43497a2fa3dd0db5cf2d0530a
TYPHOON345 <limkiran0@gmail.com>
2022-07-04 19:22:40 +0800

    added required modules

--- Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
+++ Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
@@ -1,11 +1,14 @@
 
 #note: this script runs in the background, and only works on android dev
 from jnius import autoclass
-
+from PIL import ImageGrab
+from kivy.core.window import Window
+import numpy as np
+import cv2
 
 antispamservice = autoclass('org.kivy.android.antispamservice')
 antispamservice.mService.setAutoRestartService(True)
 
 while True:
     print("checking if whatsapp is open")
-    
+    print(Window.size)
