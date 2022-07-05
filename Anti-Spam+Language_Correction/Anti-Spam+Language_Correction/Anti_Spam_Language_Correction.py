e9b17a5af301a31a92550c7c474807e09d3e9a3f
TYPHOON345 <limkiran0@gmail.com>
2022-07-05 16:37:03 +0800

    added screen recorder

--- Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
+++ Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
@@ -1,11 +1,22 @@
 
 #note: this script runs in the background, and only works on android dev
+from curses import window
+from typing import final
 from PIL import ImageGrab
 from kivy.core.window import Window
 import numpy as np
 import cv2
 
+width = Window.size[0]
+height = Window.size[1]
+
 if __name__ == '__main__':
     while True:
         #setup OCR and check if whatsapp is open
         print("checking if whatsapp is open")
+        img = ImageGrab.grab(bbox=(0, 0, width, height))
+        img_np = np.array(img)
+        finalimg = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+        cv2.imshow("Not OBS", finalimg)
+        
+        
