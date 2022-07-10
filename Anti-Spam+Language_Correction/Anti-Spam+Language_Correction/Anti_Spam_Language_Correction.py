1a564adc0bff0ee478f5ca65c1937950d004f98f
TYPHOON345 <limkiran0@gmail.com>
2022-07-10 15:19:41 +0800

    added template matching

--- Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
+++ Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
@@ -1,23 +1,60 @@
 
-#note: this script runs in the background, and only works on android dev
+#note: this script runs in the background, and only works on android, mac or windows
 from curses import window
+from re import template
+from tkinter.tix import Tree
+from turtle import width
 from typing import final
+from winsound import PlaySound
 from PIL import ImageGrab
+from bs4 import ResultSet
+import kivy #importing necessary libraries for OCR and screen recording
 from kivy.core.window import Window
+from kivy.utils import platform
 import numpy as np
 import cv2
+import pytesseract
 
-width = Window.size[0]
-height = Window.size[1]
+if platform == 'win':
+    import pyautogui
+    width, height = pyautogui.size()
+if platform == 'macosx':
+    from tkinter import Tk
+
+    root = Tk()
+    height = root.winfo_screenheight()
+    wdith = root.winfo_screenwidth()
+
+if platform == 'android':
+    width = Window.size(0)
+    height = Window.size(1)
+
+
+testimage = cv2.imread('assets/testimage.png', 0)
+desktoptemplateimage = cv2.imread('assets/win_template_img.png', 0) #load all template images from assets folder in greyscale
+mobiletemplateimage = cv2.imread('assets/android_light_template_img.png', 0)   
+
+didlaunchwhatsapp = False
 
 if __name__ == '__main__':
     while True:
-        #setup OCR and check if whatsapp is open
-        print("checking if whatsapp is open")
-        img = ImageGrab.grab(bbox=(0, 0, width, height))
-        img_np = np.array(img)
-        finalimg = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-        cv2.imshow("Not OBS", finalimg)
-        cv2.waitKey(10)
+        #setup screen recording
+        img = ImageGrab.grab(bbox=(0, 0, width, height)) #using PIllow to take an image of the user's screen
+        img_np = np.array(img) #converts image to numpy array to be processed by opencv
+        finalimg = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY) #converts image to RGB
+
+        #OCR
+        if platform == 'win' or platform == 'macosx':
+            
+            h, w = desktoptemplateimage.shape
+        if platform == 'android':
+            h, w = mobiletemplateimage.shape
+        
+        imagechecked = finalimg.copy()
+        result = cv2.matchTemplate(imagechecked, desktoptemplateimage, cv2.TM_CCOEFF_NORMED)
+        locations = np.where(result >= 0.8)
+        print(list(zip(*locations[::-1])))
+        if list(zip(*locations[::-1])).count > 0:
+            didlaunchwhatsapp = True
         
         
