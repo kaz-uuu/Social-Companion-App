6c68a1f4bbb2138376e3ce353739f0c0f17b7c1e
kaz_uuu <kilozulu.eleven@gmail.com>
2022-09-17 10:23:36 +0800

    modified speech recognition

--- MaintrainingApp/MaintrainingApp.py
+++ MaintrainingApp/MaintrainingApp.py
@@ -2,7 +2,7 @@ from re import U
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from jnius import autoclass
+#from jnius import autoclass
 import speech_recognition
 import pyttsx3
 import pyaudio
@@ -19,21 +19,20 @@ class trainingApp(App): #this is the main training app that is going to be downl
         def recognizeSpeech(self):
             print("Starting Recording")
             recognizer = speech_recognition.Recognizer() #start recognizing speech
-            with speech_recognition.Microphone() as mic:
-                print("speak anything")
-                recognizer.adjust_for_ambient_noise(mic,duration=5)
-                audio = recognizer.listen(mic)
-
-                try: 
+            print("speak anything")
+            try: 
+                with speech_recognition.Microphone() as mic:
+                    recognizer.adjust_for_ambient_noise(mic,duration=1)
+                    audio = recognizer.listen(mic)
                     text = recognizer.recognize_google(audio)
                     text = text.lower()
                     print(text)
-                except:
-                    print("could not recognize your voice")
+            except speech_recognition.UnknownValueError():
+                recognizer.adjust_for_ambient_noise(mic,duration=)
+                print("could not recognize your voice")
                 
     
     def startantispam(self): #this function starts the antispam and language corrector as a background service
-        from jnius import autoclass
         antispamservice = autoclass(SERVICE_NAME)
         mActivity = autoclass(u'org.kivy.android.PythonActivity').mActivity
         antispamservice.start(mActivity,'')
@@ -48,5 +47,4 @@ class trainingApp(App): #this is the main training app that is going to be downl
 if __name__=="__main__":
     app = trainingApp()
     app.run()
-    app.startantispam()
 
