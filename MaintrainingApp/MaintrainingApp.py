8199b64a14da7d070362cc6f292300570f37ffeb
kaz_uuu <kilozulu.eleven@gmail.com>
2022-09-17 10:01:44 +0800

    added speech recognition

--- MaintrainingApp/MaintrainingApp.py
+++ MaintrainingApp/MaintrainingApp.py
@@ -2,6 +2,10 @@ from re import U
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from jnius import autoclass
+import speech_recognition
+import pyttsx3
+import pyaudio
 
 
 SERVICE_NAME = u'{packagename}.Service{servicename}'.format(
@@ -11,7 +15,24 @@ SERVICE_NAME = u'{packagename}.Service{servicename}'.format(
 class trainingApp(App): #this is the main training app that is going to be downloaded into the user's phone
     #add code for training app here, free to change name
     
-    def startantispam(): #this function starts the antispam and language corrector as a background service
+    class mainsimulator():
+        def recognizeSpeech(self):
+            print("Starting Recording")
+            recognizer = speech_recognition.Recognizer() #start recognizing speech
+            with speech_recognition.Microphone() as mic:
+                print("speak anything")
+                recognizer.adjust_for_ambient_noise(mic,duration=5)
+                audio = recognizer.listen(mic)
+
+                try: 
+                    text = recognizer.recognize_google(audio)
+                    text = text.lower()
+                    print(text)
+                except:
+                    print("could not recognize your voice")
+                
+    
+    def startantispam(self): #this function starts the antispam and language corrector as a background service
         from jnius import autoclass
         antispamservice = autoclass(SERVICE_NAME)
         mActivity = autoclass(u'org.kivy.android.PythonActivity').mActivity
@@ -20,7 +41,12 @@ class trainingApp(App): #this is the main training app that is going to be downl
 
     def build(self):
         return Button(text='hello world')
-        
+    
+    mainsim = mainsimulator()
+    mainsim.recognizeSpeech()
+
 if __name__=="__main__":
     app = trainingApp()
     app.run()
+    app.startantispam()
+
