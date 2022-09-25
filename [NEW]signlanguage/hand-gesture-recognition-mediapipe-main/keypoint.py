7deba4c253fa4688e55a01c29a6cd04bcef43fc3
yanzhaoliu <88018845+yanzhaoliu@users.noreply.github.com>
2022-09-25 17:01:21 +0800

    kivy & ML
    it works !!!

---
+++ [NEW]signlanguage/hand-gesture-recognition-mediapipe-main/keypoint.py
@@ -0,0 +1,170 @@
+def train():
+        # %%
+    import csv
+
+    import numpy as np
+    import tensorflow as tf
+    from sklearn.model_selection import train_test_split
+
+    RANDOM_SEED = 42
+
+    # %% [markdown]
+    # # Specify each path
+
+    # %%
+    dataset = '/Users/liuyanzhao/Documents/GitHub/Tech4Good/[NEW]signlanguage/hand-gesture-recognition-mediapipe-main/model/keypoint_classifier/keypoint.csv'
+    model_save_path = '/Users/liuyanzhao/Documents/GitHub/Tech4Good/[NEW]signlanguage/hand-gesture-recognition-mediapipe-main/model/keypoint_classifier/keypoint_classifier.hdf5'
+    tflite_save_path = '/Users/liuyanzhao/Documents/GitHub/Tech4Good/[NEW]signlanguage/hand-gesture-recognition-mediapipe-main/model/keypoint_classifier/keypoint_classifier.tflite'
+
+    # %% [markdown]
+    # # Set number of classes
+
+    # %%
+    NUM_CLASSES = 34
+
+    # %% [markdown]
+    # # Dataset reading
+
+    # %%
+    X_dataset = np.loadtxt(dataset, delimiter=',', dtype='float32', usecols=list(range(1, (21 * 2) + 1)))
+
+    # %%
+    y_dataset = np.loadtxt(dataset, delimiter=',', dtype='int32', usecols=(0))
+
+    # %%
+    X_train, X_test, y_train, y_test = train_test_split(X_dataset, y_dataset, train_size=0.75, random_state=RANDOM_SEED)
+
+    # %% [markdown]
+    # # Model building
+
+    # %%
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Input((21 * 2, )),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(20, activation='relu'),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
+    ])
+
+    # %%
+    model.summary()  # tf.keras.utils.plot_model(model, show_shapes=True)
+
+    # %%
+    # Model checkpoint callback
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        model_save_path, verbose=1, save_weights_only=False)
+    # Callback for early stopping
+    es_callback = tf.keras.callbacks.EarlyStopping(patience=20, verbose=1)
+
+    # %%
+    # Model compilation
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    # %% [markdown]
+    # # Model training
+
+    # %%
+    model.fit(
+        X_train,
+        y_train,
+        epochs=1000,
+        batch_size=128,
+        validation_data=(X_test, y_test),
+        callbacks=[cp_callback, es_callback]
+    )
+
+    # %%
+    # Model evaluation
+    val_loss, val_acc = model.evaluate(X_test, y_test, batch_size=128)
+
+    # %%
+    # Loading the saved model
+    model = tf.keras.models.load_model(model_save_path)
+
+    # %%
+    # Inference test
+    predict_result = model.predict(np.array([X_test[0]]))
+    print(np.squeeze(predict_result))
+    print(np.argmax(np.squeeze(predict_result)))
+
+    # %% [markdown]
+    # # Confusion matrix
+
+    # %%
+    import pandas as pd
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import confusion_matrix, classification_report
+
+    def print_confusion_matrix(y_true, y_pred, report=True):
+        labels = sorted(list(set(y_true)))
+        cmx_data = confusion_matrix(y_true, y_pred, labels=labels)
+        
+        df_cmx = pd.DataFrame(cmx_data, index=labels, columns=labels)
+    
+        fig, ax = plt.subplots(figsize=(7, 6))
+        sns.heatmap(df_cmx, annot=True, fmt='g' ,square=False)
+        ax.set_ylim(len(set(y_true)), 0)
+        plt.show()
+        heatmap = plt
+        
+        if report:
+            # print('Classification Report')
+            # print(classification_report(y_test, y_pred))
+            return (classification_report(y_test, y_pred))
+
+    Y_pred = model.predict(X_test)
+    y_pred = np.argmax(Y_pred, axis=1)
+
+    report = print_confusion_matrix(y_test, y_pred)
+
+    # %% [markdown]
+    # # Convert to model for Tensorflow-Lite
+
+    # %%
+    # Save as a model dedicated to inference
+    model.save(model_save_path, include_optimizer=False)
+
+    # %%
+    # Transform model (quantization)
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    tflite_quantized_model = converter.convert()
+
+    open(tflite_save_path, 'wb').write(tflite_quantized_model)
+
+    # %% [markdown]
+    # # Inference test
+
+    # %%
+    interpreter = tf.lite.Interpreter(model_path=tflite_save_path)
+    interpreter.allocate_tensors()
+
+    # %%
+    # Get I / O tensor
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # %%
+    interpreter.set_tensor(input_details[0]['index'], np.array([X_test[0]]))
+
+    # %%
+    # %%time
+    # Inference implementation
+    interpreter.invoke()
+    tflite_results = interpreter.get_tensor(output_details[0]['index'])
+
+    # %%
+    print(np.squeeze(tflite_results))
+    print(np.argmax(np.squeeze(tflite_results)))
+
+    return report, plt
+
+
+
--- [NEW]signlanguage/hand-gesture-recognition-mediapipe-main/kivyapp.py
+++ [NEW]signlanguage/hand-gesture-recognition-mediapipe-main/kivyapp.py
@@ -4,17 +4,17 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDRaisedButton
 from kivy.uix.image import Image
 from kivy.clock import Clock
+from kivy.core.window import Window
 from kivy.graphics.texture import Texture
 
 import cv2 as cv
 import app
+import keypoint
 import time
-
-import copy
-from collections import Counter
 
 
 KV = '''
+
 MDScreen:
     MDBottomNavigation:
         #panel_color: "#eeeaea"
@@ -35,49 +35,114 @@ MDScreen:
                     pos_hint: {"center_x": .5, "center_y": .5}
         
         MDBottomNavigationItem:
-            name: 'screen 2'
+            id: screen2
+            name: 'screen2'
             text: 'Training Sign Language'
-            MDTextField:
-                hint_text: "Name"
-                helper_text: "Name of Sign Language"
-                helper_text_mode: "persistent"
+            MDBoxLayout:
+                id: box
+                orientation: "vertical"
+                pos_hint: {'center_y':0.2}
+                adaptive_height: True
+                spacing: 20
+                MDTextField:
+                    id: name
+                    hint_text: "Name"
+                    helper_text: "Name of Sign Language"
+                    helper_text_mode: "persistent"
+                MDTextField:
+                    id: slot
+                    hint_text: "Slot"
+                    helper_text: "Pick a slot (1~10)"
+                    helper_text_mode: "persistent"
+                MDRectangleFlatIconButton:
+                    id: mdbu
+                    text: "Start Training!"
+                    on_press: app.train()
+                    adaptive_size: True
+                MDLabel:
+                    id: label
+                MDLabel:
+                    id: label
+                
+            
 '''
 
 class myCam(MDApp):
 
+    # Initial Set-Up #####################################################
     def build(self):
+        Window.size = (450,975)
         self.camera = 0
+        self.key = 0
         self.layout = MDBoxLayout(orientation='vertical')
         self.theme_cls.material_style = "M3"
         self.theme_cls.theme_style = "Dark"
         return Builder.load_string( KV ) 
 
-
+    # Main Image Reconition #####################################################
     def main(self):
+        # On #####################################################
         if self.camera == 0:
             self.image = Image()
-            # self.cap = cv.VideoCapture(0)
-            self.mode, self.use_brect, self.hands, self.keypoint_classifier, self.cvFpsCalc, self.point_history, self.finger_gesture_history, self.keypoint_classifier_labels, self.cap, self.number = app.main()
+            self.use_brect, self.hands, self.keypoint_classifier, self.cvFpsCalc, self.point_history, self.finger_gesture_history, self.keypoint_classifier_labels, self.cap = app.main()
+            self.number = None
+            self.data = None
+            self.mode = 0
             self.root.ids.screen1.add_widget(self.image)
-            # while True:
-                # Process Key (ESC: end) #################################################
-                # key = cv.waitKey(10)
-                # if key == 27:  # ESC
-                #     cap.release()
-                #     cv.destroyAllWindows()
-                # number, mode = app.select_mode(key, mode)
             Clock.schedule_interval(self.load_video, 1.0/33.0)
+            self.camera = 1
+        # Off #####################################################
         elif self.camera == 1:
             self.camera = 0
+            self.key = 1
+            Clock.unschedule(self.load_video)
+            Clock.schedule_once(self.load_video, -1)
             self.root.ids.screen1.remove_widget(self.image)
-            # self.cap.release()
+
+    # Training SL Model #####################################################
+    def train(self):
+        if self.camera == 0:
+            name = self.root.ids.name.text
+            slot = int(self.root.ids.slot.text)
+            label = []
+            file = "/Users/liuyanzhao/Documents/GitHub/Tech4Good/[NEW]signlanguage/hand-gesture-recognition-mediapipe-main/model/keypoint_classifier/keypoint_classifier_label.csv"
+            with open(file, "r") as fin:
+                for _ in range(34):
+                    label.append(fin.readline().strip("\n"))      
+            label[24+slot-1] = name
+            with open(file, "w") as fout:
+                fout.write("\n".join(label))
+            self.root.ids.name.text = ""
+            self.root.ids.slot.text = ""
+
+            self.image = Image()
+            self.use_brect, self.hands, self.keypoint_classifier, self.cvFpsCalc, self.point_history, self.finger_gesture_history, self.keypoint_classifier_labels, self.cap = app.main()
+            self.number = slot
+            self.mode = 1
+            self.data = 0
+            self.root.ids.screen2.add_widget(self.image)
+            Clock.schedule_interval(self.load_video, 1.0/10.0)
+            self.camera = 1
+        elif self.camera == 1:
+            self.camera = 0
+            self.key = 1
+            Clock.unschedule(self.load_video)
+            Clock.schedule_once(self.load_video, -1)
+            self.root.ids.screen2.remove_widget(self.image)
+            time.sleep(1)
+            report , plt= keypoint.train()
+            print("REPORT")
+            print(report)
+
         
     def load_video(self, *args):
-        img = app.loading(self.mode, self.use_brect, self.hands, self.keypoint_classifier, self.cvFpsCalc, self.point_history, self.finger_gesture_history, self.keypoint_classifier_labels, self.cap, self.number)
+        img, self.data = app.loading(self.mode, self.use_brect, self.hands, self.keypoint_classifier, self.cvFpsCalc, self.point_history, self.finger_gesture_history, self.keypoint_classifier_labels, self.cap, self.number, self.key, self.data)
         # cv.imshow('Hand Gesture Recognition', img)
         buffer = cv.flip(img,0).tostring()
         texture1 = Texture.create(size=(img.shape[1],img.shape[0]), colorfmt='bgr')
         texture1.blit_buffer(buffer, colorfmt='bgr',bufferfmt='ubyte')
         self.image.texture = texture1
+        self.root.ids.label.text = "Number of data collected: {}".format(str(self.data))
+        
 
 myCam().run()
