6ef65e36e9007d6fcfb5ae2c9e3ea772f7553839
TYPHOON345 <limkiran0@gmail.com>
2022-09-25 11:02:11 +0800

    finished anti-Spam for mac and windows

--- Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
+++ Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
@@ -1,9 +1,8 @@
 
 #note: this script runs in the background, and only works on android, mac or windows
-from curses import window
-from logging.config import listen
-from multiprocessing.connection import Listener
-from socket import timeout
+import threading
+from time import time
+from types import NoneType
 from PIL import ImageGrab
 import kivy #importing necessary libraries for OCR and screen recording
 from kivy.utils import platform
@@ -11,19 +10,19 @@ import numpy as np
 import cv2
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-from threading import Event
+from threading import Event, Timer
+import keyboard as Keyboard
 AntiSpamEnabled = True #Check if the script has been enabled
+
 
 
 if platform == 'win': #detect the platform that this script is being run on ,and set the width and height of the image to be captured accordingly.
     import pyautogui
     width, height = pyautogui.size() # pyautogui is used because it is meant for windows, so goes for tkinter and kivy.Window
 if platform == 'macosx':
-    from tkinter import Tk
-
-    root = Tk()
-    height = root.winfo_screenheight()
-    width = root.winfo_screenwidth()
+    width = 2880
+    height = 1800
+    print("width: {}, height: {}".format(width, height))
 
 if platform == 'android':
     from kivy.core.window import Window
@@ -33,11 +32,12 @@ if platform == 'android':
 #reading template images from assets, desktop templates are best stored in an array since there are two of them.
 desktoptemplates = [cv2.imread('assets/win_template_img.png', 0), cv2.imread('assets/win_template_typing_img.png', 0)]
 mobiletemplateimages = [cv2.imread('assets/android_template_idle_img.png'), cv2.imread('assets/android_template_typing.png'), cv2.imread('assets/android_textfield_template.png')]
-mactemplates = [cv2.imread('assets/macosx_template_idle_img.png', 0), cv2.imread('assets/macosx_template_img.png', 0)]
-
+mactemplates = [cv2.imread('assets/macosx_template_img.png', 0)]
+sendbuttontemplate = cv2.imread('assets/android_send_button.png', 0)
 
 messagecounter = 0 #counter for how many messages the user has sent in a short period of time
 messagestring = ""
+didFinishTimer = False
 from pynput import keyboard
 def startscreenrecorder():
         global didlaunchwhatsapp
@@ -56,9 +56,10 @@ def startscreenrecorder():
                     locations = np.where(results >= 0.75) # threshold is used so as to ensure that detection is accurate.
             if platform == "macosx":
                 for template in mactemplates:
+                    
                     h,w = template.shape
                     results = cv2.matchTemplate(imagechecked, template, cv2.TM_CCOEFF_NORMED)
-                    locations = np.where(results >= 0.75)
+                    locations = np.where(results >= 0.9)
             if platform == 'android':
                 for template in mobiletemplateimages: 
                     h, w = template.shape
@@ -66,30 +67,63 @@ def startscreenrecorder():
                     locations = np.where(results >= 0.75) # threshold is used so as to ensure that detection is accurate.
         
             if (list(zip(*locations[::-1])) != []): #if there are matches of the template found in the orignal image
-                if platform == "win" or platform == "mac":
+                if platform == "win" or platform == "macosx":
                     listener = keyboard.Listener(on_release=on_release)
                 if platform == "android":
                     from AndroidKeyboardListener import keyboardlistener
                     listener = keyboardlistener()
+                    buttonresults = cv2.matchTemplate(imagechecked, sendbuttontemplate, cv2.TM_CCOEFF_NORMED)
+                    buttonlocations = np.where(buttonresults > 0.8)
+                    if (list(zip(*buttonlocations[::-1])) != []):
+                        h, w = sendbuttontemplate.shape
                 listener.daemon = True
                 listener.start()
                 listener.join()
-            
+                
             if (list(zip(*locations[::-1])) == []):
                 didlaunchwhatsapp = False
-            print(list(zip(*locations[::-1])))
+                
 def on_release(key):
     global messagestring
-    if hasattr(key, 'char'):
+    if hasattr(key, 'char') and key.char != None:
         messagestring += key.char
     if key == keyboard.Key.backspace: 
         messagestring = messagestring[:-1]
     if key == keyboard.Key.space:
         messagestring += " "
-    if key == keyboard.Key.enter or key == keyboard.Key.alt:
-        return False
+    if key == keyboard.Key.enter:
+        if platform == "macosx":
+
+            import time
+            timeleft = 10
+            controller = keyboard.Controller()
+            while timeleft:
+                mins, sec = divmod(timeleft, 60)
+                time.sleep(0.001)
+                print("Second passed")
+                controller.press(keyboard.Key.backspace)
+                if timeleft <= 0:
+                    break
+                timeleft -= 0.001
+                print("Timeleft: {}".format(timeleft))
+        if platform == "win":
+            import time
+            timeleft = 10
+
+            while timeleft:
+                mins, sec = divmod(timeleft, 60)
+                time.sleep(0.1)
+                print("Second passed")
+                Keyboard.block_key("ENTER")
+                if timeleft <= 0:
+                    break
+                timeleft -= 0.1
+                print("Timeleft: {}".format(timeleft))
+            Keyboard.unblock_key("ENTER")
     print(messagestring)        
-        
+def releasekeyboard():
+    global didFinishTimer
+    didFinishTimer = True
 if __name__ == '__main__':
     from threading import Thread
     from threading import Event
