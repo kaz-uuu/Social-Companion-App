6426e4bc87fc97e3882f45a26ad04bcecf8c95e8
TYPHOON345 <limkiran0@gmail.com>
2023-01-26 13:38:31 +0800

    fixed code

--- Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
+++ Anti-Spam+Language_Correction/Anti-Spam+Language_Correction/Anti_Spam_Language_Correction.py
@@ -1,9 +1,9 @@
 
-#note: this script runs in the background, and only works on android, mac or windows
-from operator import truediv
+#note: this script runs in the background, and only works on mac or windows
 import threading
 from time import time
 from tkinter import Tk
+from urllib.parse import urlencode
 from PIL import ImageGrab
 import kivy #importing necessary libraries for OCR and screen recording
 from kivy.utils import platform
@@ -19,16 +19,17 @@ from androidAutomate import Device
 AntiSpamEnabled = True #Check if the script has been enable
 if platform == 'win': #detect the platform that this script is being run on ,and set the width and height of the image to be captured accordingly.
     import pyautogui
-    width, height = pyautogui.size() # pyautogui is used because it is meant for windows, so goes for tkinter and kivy.Window
+    width, height = pyautogui.size() # pyautogui is used because it is meant for windows
     print("width: {}, height: {}".format(width, height))
     desktoptemplates = [cv2.imread('assets/win_template_img.png', 0), cv2.imread('assets/win_template_typing_img.png', 0)]
 if platform == 'macosx':
-    root = Tk()
-    width = root.winfo_screenwidth()
+    root = Tk() #Tkinter is used for macosx. Initialise a new Tk object to access the necessary width and height information of the local mac machine
+    width = root.winfo_screenwidth() #get the width and height of the screen from the Tk object and store it in variables
     height = root.winfo_screenheight()
-    print("width: {}, height: {}".format(width, height))
-    mactemplates = [cv2.resize(cv2.imread('assets/macosx_template_img.png', 0), (0, 0), fx= (width / 2880), fy=(height / 1880))]
+    print("width: {}, height: {}".format(width, height)) # intermittent print statement for debugging
+    mactemplates = [cv2.resize(cv2.imread('assets/macosx_template_img.png', 0), (0, 0), fx= (width / 2880), fy=(height / 1880))] #reading the template image for mac from the assets file using opencv, and resize the image to fit the actual dimensions on the screen(2880 x 1880)
 
+#android is still a work in progress, not ready yet
 if platform == 'android':
     from kivy.core.window import Window
     width = Window.size(0)
@@ -39,15 +40,15 @@ if platform == 'android':
 
 
 messagecounter = 0 #counter for how many messages the user has sent in a short period of time
-messagestring = ""
-didFinishTimer = False
+messagestring = "" #string varaible to store what message the user is typing in whatsapp
+didFinishTimer = False #status variables to store 
 didtypemessage = False
 from pynput import keyboard
 def startscreenrecorder():
-        localdevice = Device(deviceId=True)
         global didlaunchwhatsapp
         while AntiSpamEnabled:
             #setup screen recording
+            
             img = ImageGrab.grab(bbox=(0, 0, width, height)) #using PIllow to take an image of the user's screen
             img_np = np.array(img) #converts image to numpy array to be processed by opencv
             finalimg = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY) #converts image to GrayScale
@@ -57,14 +58,15 @@ def startscreenrecorder():
             if platform == 'win': #if this script is being run on a desktop/laptop, get the width and height of the template image
                 for template in desktoptemplates: 
                     h, w = template.shape
-                    results = cv2.matchTemplate(imagechecked, template, cv2.TM_CCOEFF_NORMED) #match templates for the copy of the image
+                    results = cv2.matchTemplate(imagechecked, template, cv2.TM_CCOEFF_NORMED) #match templates for the copy of the image, TM_CCOEFFF_NORMED is the algorithm of choice as it has been tested by me to be the most reliable 
                     locations = np.where(results >= 0.75) # threshold is used so as to ensure that detection is accurate.
             if platform == "macosx":
                 for template in mactemplates:
-                    global didtypemessage
-                    h,w = template.shape
-                    results = cv2.matchTemplate(imagechecked, template, cv2.TM_CCOEFF_NORMED)
-                    locations = np.where(results >= 0.9)
+                    global didtypemessage # Declare the status variable as a global variable, so that I can change this varaible from inside a function
+                    h,w = template.shape #getting the height and width of the template that currently being checked for based off their "shape" property
+                    results = cv2.matchTemplate(imagechecked, template, cv2.TM_CCOEFF_NORMED) #use template matching to check for a copy of the template image within the screenshot of the user's screen
+                    locations = np.where(results >= 0.9) #getting the coordinates of the matches with a confidence score greater than equals to 0.9
+            #android is still a work in progress
             if platform == 'android':
                 for template in mobiletemplateimages: 
                     h, w = template.shape
@@ -72,8 +74,10 @@ def startscreenrecorder():
                     locations = np.where(results >= 0.75) # threshold is used so as to ensure that detection is accurate.
         
             if (list(zip(*locations[::-1])) != []): #if there are matches of the template found in the orignal image
+                
                 if platform == "win" or platform == "macosx":
-                    listener = keyboard.Listener(on_release=on_release)
+                    listener = keyboard.Listener(on_release=on_release) #start a keylogger on a seperate thread to track what the user is typing, and if he has sent a message etc
+                #android is still a work in progress
                 if platform == "android":
                     from AndroidKeyboardListener import keyboardlistener
                     listener = keyboardlistener()
@@ -93,64 +97,70 @@ def startscreenrecorder():
                             timeleft = 10
                             while timeleft:
                                 time.sleep(0.1)
-                                Keyboard.hook_key("ENTER", lambda e: False, suppress=True)
                                 if timeleft <= 0:
-                                    Keyboard.unhook_all()
+
                                     break
                                 timeleft -= 0.1
                         
-                        
+                #start and join the keyboard listener thread        
                 listener.daemon = True
                 listener.start()
                 listener.join()
                 
             if (list(zip(*locations[::-1])) == []):
                 didlaunchwhatsapp = False
-
+#function to execute when a key is pressed and released(when a new letter is pressed)
 def on_release(key):
-    global messagestring
+    global messagestring #set the messagestring and didlaucnhwhatsapp variables as global variables to access them from inside the function
     global didlaunchwhatsapp
-    if hasattr(key, 'char') and key.char != None:
-        messagestring += key.char
-    if key == keyboard.Key.backspace: 
-        messagestring = messagestring[:-1]
-    if key == keyboard.Key.space:
-        messagestring += " "
+
+    #This is to update the messagestring variable according to what the user would have typed in the whatsapp 
+    if hasattr(key, 'char') and key.char != None: #if the key pressed is a character
+        messagestring += key.char #add the character pressed into the entire message string variable
+    if key == keyboard.Key.backspace: #if the user pressed backspace
+        messagestring = messagestring[:-1]#remove the last character from the messagestring
+    if key == keyboard.Key.space: #if the user pressed space
+        messagestring += " "#add a space to the messagestring
     if key == keyboard.Key.enter:
-
-
+        #if the user has pressed the enter key
         if platform == "macosx":
 
             import time
-            timeleft = 10
-            controller = keyboard.Controller()
+            timeleft = 10#set the timeleft on the timer in seconds to 10
+            controller = keyboard.Controller() #Starting a keyboard controller object to delete any key the user presses during that 10 seconds
             while timeleft:
-                mins, sec = divmod(timeleft, 60)
-                time.sleep(0.001)
+                time.sleep(0.001)#sleep for 0.001 seconds
                 print("Second passed")
-                controller.press(keyboard.Key.backspace)
-                if timeleft <= 0:
+                controller.press(keyboard.Key.backspace)#every 1 milisecond press the backspace key to prevent the user from sending 
+                if timeleft <= 0: #if the time left is up, break out the loop, and set the status variable didFinishTimer to true
+                    global didFinishTimer
+                    didFinishTimer = True
                     break
-                timeleft -= 0.001
+                timeleft -= 0.001 #remove 1 milisecond from the time varaible
                 print("Timeleft: {}".format(timeleft))
         if platform == "win":
+            #for windows, the "hook" function fo the keyboard module is not blocekd, so we can use that to stop the enter key from being pressed
             import time
             timeleft = 10
 
             while timeleft:
                 time.sleep(0.1)
-                Keyboard.hook_key("ENTER", lambda e: False, suppress=True)
+                Keyboard.hook_key("ENTER", lambda e: False, suppress=True) #while the 10 second timer is still active, use the hook function to suppress any inputs from the Enter key(block the enter key)
                 if timeleft <= 0:
-                    Keyboard.unhook_all()
+                    Keyboard.unhook_all() #unhook the enter key and allow the user to send messages again
                     break
-                timeleft -= 0.1
-
+                timeleft -= 0.1 #remove 0.1 seconds from the timer variable
     print(messagestring)
+    
+    
 
-    if not didlaunchwhatsapp:
-        return False        
-    global didFinishTimer
-    didFinishTimer = True
+    if not didlaunchwhatsapp: #stop the thread if whatsapp isn't detected
+        return False
+
+#function for checking the message for toxicity, still work in progress
+
+    
+    
 if __name__ == '__main__':
     from threading import Thread
     from threading import Event
